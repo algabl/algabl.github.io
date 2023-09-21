809156aa546c87de414dbe973146ca5801d3fae3
algabl <103858110+algabl@users.noreply.github.com>
2023-09-20 22:35:38 -0600

    polished looking now

--- script.js
+++ script.js
@@ -1,50 +1,54 @@
 document.addEventListener('DOMContentLoaded',function(event){
+
+  var isAnimationRunning = false;
   // array with texts to type in typewriter
   var dataText = [ "Hello world.", "I'm Alex Black.", "An information systems student at Brigham Young University."];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    
-    var cssClass = "";
- 
-    if (text.substring(i-1) == " ") {
-      cssClass += 'nord-green';
-    }
-    
-    if (i < (text.length)) {
-      // add next character to h1
-     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-     document.querySelector("h1").setAttribute('class', cssClass);
-    
-      // wait for a while and call this function again for next character
+  function typeWriter(text, i, callback) {   
+    if ( i < text.length ) {
+      document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
       setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback)
+        typeWriter(text, i +1, callback);
       }, 100);
+    } else {
+      if (typeof callback === 'function') {
+        callback();
+      }
     }
+   
     // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
-      // call callback after timeout
-      setTimeout(fnCallback, 700);
-    }
   }
   // start a typewriter animation for a text in the dataText array
-   function StartTextAnimation(i) {
-     if (typeof dataText[i] == 'undefined'){
-        setTimeout(function() {
-          StartTextAnimation(0);
-        }, 20000);
-     }
-     // check if dataText[i] exists
-    if (i < dataText[i].length) {
-      // text exists! start typewriter animation
-     typeWriter(dataText[i], 0, function(){
-       // after callback (and whole text has been animated), start next text
-       StartTextAnimation(i + 1);
-     });
+   function StartTextAnimation() {
+    isAnimationRunning = true;
+    var index = 0;
+    function animateNextText() {
+      if (index < dataText.length) {
+        typeWriter(dataText[index], 0, function() {
+          setTimeout(function() {
+            document.querySelector("h1").textContent = "";
+            index ++;
+            animateNextText();
+          }, 1000);
+        });
+      } else {
+        isAnimationRunning = false;
+        document.querySelector("h1").innerHTML = dataText[dataText.length -1] + ' <span aria-hidden="true"></span>';
+      }
     }
+    animateNextText();
   }
   // start the text animation
-  StartTextAnimation(0);
+  StartTextAnimation();
+  
+  var replayButton = document.getElementById('replayButton');
+
+  replayButton.addEventListener('click', function() {
+    if (!isAnimationRunning) {
+      document.querySelector("h1").textContent = "";
+      StartTextAnimation();
+    }
+  });
 });
