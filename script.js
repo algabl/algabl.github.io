74f57d1b41a2d6cc72b41a9d7484a18c67d8a17a
algabl <103858110+algabl@users.noreply.github.com>
2024-02-24 00:34:56 -0700

    some big updates

--- script.js
+++ script.js
@@ -1,75 +1,123 @@
-document.addEventListener('DOMContentLoaded',function(event){
+var isAnimationRunning = false;
 
-  var isAnimationRunning = false;
-  // array with texts to type in typewriter
-  var dataText = [ "print('Hello world!');", "# I'm Alex Black", "# An information systems student at Brigham Young University"];
-  
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, callback) {   
-    if ( i < text.length ) {
-      document.getElementById(text).innerHTML = '<h1>' + text.substring(0, i+1) +'<span aria-hidden="true"></span></h1>';
-      setTimeout(function() {
-        typeWriter(text, i +1, callback);
-      }, 40);
+
+function typeWriter(elementId, text, i, callback) {   
+  if ( i <= text.length ) {
+    document.getElementById(elementId).innerHTML = '<h1>' + text.substring(0, i+1) +'<span aria-hidden="true"></span></h1>';
+    setTimeout(function() {
+      typeWriter(elementId, text, i +1, callback);
+    }, 40);
+  } else {
+    if (typeof callback === 'function') {
+      callback();
+    }
+  }
+ 
+  // text finished, call callback if there is a callback function
+}
+
+function StartTextAnimation(dataText) {
+  isAnimationRunning = true;
+  var index = 0;
+  var funstuffDiv = document.getElementById("funstuff");
+  function animateNextText() {
+    if (index < dataText.length) {
+      var elementId = 'text' + index;
+      document.getElementById("funstuff").innerHTML += '<div class="row justify-content-center"><div class="col-1 pt-2"><h3 class="gray">' + (index + 1) + '</h3></div><div class="col-11 col-sm-9 col-md-6"><div id="' + elementId + '"></div></div></div>'
+      if (index > 0) {
+        document.getElementById('text' + (index - 1)).innerHTML = '<h1>' + dataText[index-1] +'</h1>';
+      }
+      typeWriter(elementId, dataText[index], 0, function() {
+        setTimeout(function() {
+          
+          index ++;
+          animateNextText();
+        }, 500);
+      });
+      funstuffDiv.scrollTop = funstuffDiv.scrollHeight;
     } else {
-      if (typeof callback === 'function') {
-        callback();
-      }
+      isAnimationRunning = false;
+      // document.querySelector("h1").innerHTML = dataText[dataText.length -1] + ' <span aria-hidden="true"></span>';
     }
-   
-    // text finished, call callback if there is a callback function
   }
-  // start a typewriter animation for a text in the dataText array
-   function StartTextAnimation() {
-    isAnimationRunning = true;
-    var index = 0;
-    function animateNextText() {
-      if (index < dataText.length) {
-        document.getElementById("funstuff").innerHTML += '<div class="row justify-content-center"><div class="col-1 pt-2"><h3 class="gray">' + (index + 1) + '</h3></div><div class="col-11 col-sm-9 col-md-6"><div id="' + dataText[index] + '"></div></div></div>'
-        if (index > 0) {
-          document.getElementById(dataText[index-1]).innerHTML = '<h1>' + dataText[index-1] +'</h1>';
-        }
-        typeWriter(dataText[index], 0, function() {
-          setTimeout(function() {
-            
-            index ++;
-            animateNextText();
-          }, 500);
-        });
-      } else {
-        isAnimationRunning = false;
-        // document.querySelector("h1").innerHTML = dataText[dataText.length -1] + ' <span aria-hidden="true"></span>';
-      }
+  animateNextText();
+}
+
+function toggleFade(element) {
+  if (element.isOpen) {
+    element.classList.add('floating-up-reverse');
+    element.classList.add('hidden');
+  } else {
+    element.classList.add('floating-up');
+    element.classList.remove('hidden');
+    element.classList.remove('floating-up-reverse');
+  }
+  element.isOpen = !element.isOpen;
+}
+
+function submitCustomText() {
+  if (!isAnimationRunning) {
+    var customText = dataTextInput.value.split('\n');
+    if (customText.length > 0) {
+        document.getElementById("funstuff").innerHTML = '';
+        StartTextAnimation(customText);
     }
-    animateNextText();
+  } else {
+    alert('Wait your turn ;)');
   }
-  // start the text animation
-  StartTextAnimation();
+}
+
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  var dataText = [ "print('Hello world!');", "# I'm Alex Black", "# An information systems student at Brigham Young University"];
+  StartTextAnimation(dataText);
   
   var replayButton = document.getElementById('replayButton');
 
   replayButton.addEventListener('click', function() {
     if (!isAnimationRunning) {
       document.getElementById("funstuff").innerHTML = '';
-      StartTextAnimation();
+      StartTextAnimation(dataText);
     }
   });
 });
 
 
+
+// menu stuff
 const toggleButton = document.getElementById('toggleButton');
+const customButton = document.getElementById('customButton');
 const itemList = document.getElementById('itemList');
-let isAnimating = false;
+const dataTextEntry = document.getElementById('dataTextEntry');
+const submitButton = document.getElementById('dataTextSubmit');
+const dataTextInput = document.getElementById('dataTextInput');
 
 toggleButton.addEventListener('click', () => {
-  if (isAnimating) {
-    itemList.classList.add('floating-up-reverse');
-    itemList.classList.add('hidden');
-  } else {
-    itemList.classList.add('floating-up');
-    itemList.classList.remove('hidden');
-    itemList.classList.remove('floating-up-reverse');
+  if (dataTextEntry.isOpen) {
+    toggleFade(dataTextEntry);
   }
-  isAnimating = !isAnimating;
+  toggleFade(itemList);  
+});
+
+customButton.addEventListener('click', () => {
+  if (itemList.isOpen) {
+    toggleFade(itemList);
+  }
+  toggleFade(dataTextEntry);
+  dataTextInput.focus();
+});
+
+// custom text input stuff
+
+
+submitButton.addEventListener('click', () => {
+  submitCustomText();
+});
+
+dataTextInput.addEventListener('keydown', (e) => {
+  if (e.shiftKey && e.key === 'Enter') {
+    e.preventDefault();
+    submitCustomText();
+  }
 });
